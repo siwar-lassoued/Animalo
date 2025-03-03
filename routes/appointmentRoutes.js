83016e6550c6fe3547379ddc2dd6495be86780e9
neb <nebrass@tekup.de>
2025-03-03 22:58:52 +0100

    Ajout de la documentation Swagger pour les routes appointments et availabilities

--- routes/appointmentRoutes.js
+++ routes/appointmentRoutes.js
@@ -9,10 +9,165 @@ const {
 
 const router = express.Router();
 
+/**
+ * @swagger
+ * tags:
+ *   name: Appointments
+ *   description: Gestion des rendez-vous
+ */
+
+/**
+ * @swagger
+ * /appointments:
+ *   post:
+ *     summary: Créer un nouveau rendez-vous
+ *     tags: [Appointments]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - client
+ *               - professionnel
+ *               - animal
+ *               - date
+ *               - service
+ *             properties:
+ *               client:
+ *                 type: string
+ *                 description: ID du client
+ *               professionnel:
+ *                 type: string
+ *                 description: ID du professionnel
+ *               animal:
+ *                 type: string
+ *                 description: Type d'animal
+ *               race:
+ *                 type: string
+ *                 description: Race de l'animal (optionnel)
+ *               date:
+ *                 type: string
+ *                 format: date-time
+ *               service:
+ *                 type: string
+ *                 enum: [bain, tonte, soins, complet]
+ *               statut:
+ *                 type: string
+ *                 enum: [confirmé, annulé, en attente]
+ *                 default: en attente
+ *               commentaire:
+ *                 type: string
+ *     responses:
+ *       201:
+ *         description: Rendez-vous créé avec succès
+ *       400:
+ *         description: Erreur de validation des données
+ */
 router.post("/", createAppointment);
+
+/**
+ * @swagger
+ * /appointments:
+ *   get:
+ *     summary: Récupérer tous les rendez-vous
+ *     tags: [Appointments]
+ *     responses:
+ *       200:
+ *         description: Liste des rendez-vous
+ *       500:
+ *         description: Erreur serveur
+ */
 router.get("/", getAppointments);
+
+/**
+ * @swagger
+ * /appointments/{id}:
+ *   get:
+ *     summary: Récupérer un rendez-vous par ID
+ *     tags: [Appointments]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: ID du rendez-vous
+ *     responses:
+ *       200:
+ *         description: Rendez-vous trouvé
+ *       404:
+ *         description: Rendez-vous non trouvé
+ */
 router.get("/:id", getAppointmentById);
+
+/**
+ * @swagger
+ * /appointments/{id}:
+ *   put:
+ *     summary: Mettre à jour un rendez-vous
+ *     tags: [Appointments]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: ID du rendez-vous à mettre à jour
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               client:
+ *                 type: string
+ *               professionnel:
+ *                 type: string
+ *               animal:
+ *                 type: string
+ *               race:
+ *                 type: string
+ *               date:
+ *                 type: string
+ *                 format: date-time
+ *               service:
+ *                 type: string
+ *                 enum: [bain, tonte, soins, complet]
+ *               statut:
+ *                 type: string
+ *                 enum: [confirmé, annulé, en attente]
+ *               commentaire:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Rendez-vous mis à jour
+ *       404:
+ *         description: Rendez-vous non trouvé
+ */
 router.put("/:id", updateAppointment);
+
+/**
+ * @swagger
+ * /appointments/{id}:
+ *   delete:
+ *     summary: Supprimer un rendez-vous
+ *     tags: [Appointments]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: ID du rendez-vous à supprimer
+ *     responses:
+ *       200:
+ *         description: Rendez-vous supprimé
+ *       404:
+ *         description: Rendez-vous non trouvé
+ */
 router.delete("/:id", deleteAppointment);
 
-module.exports = router;
+module.exports = router
--- routes/availabilityRoutes.js
+++ routes/availabilityRoutes.js
@@ -1,12 +1,150 @@
 const express = require("express");
-const { createAvailability, getAvailabilities, getAvailabilityById, updateAvailability, deleteAvailability } = require("../controllers/availabilityController");
+const { 
+    createAvailability, 
+    getAvailabilities, 
+    getAvailabilityById, 
+    updateAvailability, 
+    deleteAvailability 
+} = require("../controllers/availabilityController");
 
 const router = express.Router();
 
+/**
+ * @swagger
+ * tags:
+ *   name: Availabilities
+ *   description: Gestion des disponibilités des professionnels
+ */
+
+/**
+ * @swagger
+ * /availabilities:
+ *   post:
+ *     summary: Ajouter une disponibilité
+ *     tags: [Availabilities]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               professionnel:
+ *                 type: string
+ *                 description: ID du professionnel
+ *               date:
+ *                 type: string
+ *                 format: date
+ *                 description: Date de la disponibilité (AAAA-MM-JJ)
+ *               heureDebut:
+ *                 type: string
+ *                 description: Heure de début (HH:mm)
+ *               heureFin:
+ *                 type: string
+ *                 description: Heure de fin (HH:mm)
+ *     responses:
+ *       201:
+ *         description: Disponibilité créée avec succès
+ *       400:
+ *         description: Erreur de validation
+ */
 router.post("/", createAvailability);
+
+/**
+ * @swagger
+ * /availabilities:
+ *   get:
+ *     summary: Récupérer toutes les disponibilités
+ *     tags: [Availabilities]
+ *     responses:
+ *       200:
+ *         description: Liste des disponibilités
+ *       500:
+ *         description: Erreur serveur
+ */
 router.get("/", getAvailabilities);
+
+/**
+ * @swagger
+ * /availabilities/{id}:
+ *   get:
+ *     summary: Récupérer une disponibilité par ID
+ *     tags: [Availabilities]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: ID de la disponibilité
+ *     responses:
+ *       200:
+ *         description: Disponibilité trouvée
+ *       404:
+ *         description: Disponibilité non trouvée
+ *       500:
+ *         description: Erreur serveur
+ */
 router.get("/:id", getAvailabilityById);
+
+/**
+ * @swagger
+ * /availabilities/{id}:
+ *   put:
+ *     summary: Mettre à jour une disponibilité
+ *     tags: [Availabilities]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: ID de la disponibilité
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               date:
+ *                 type: string
+ *                 format: date
+ *               heureDebut:
+ *                 type: string
+ *               heureFin:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Disponibilité mise à jour
+ *       404:
+ *         description: Disponibilité non trouvée
+ *       500:
+ *         description: Erreur serveur
+ */
 router.put("/:id", updateAvailability);
+
+/**
+ * @swagger
+ * /availabilities/{id}:
+ *   delete:
+ *     summary: Supprimer une disponibilité
+ *     tags: [Availabilities]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: ID de la disponibilité
+ *     responses:
+ *       200:
+ *         description: Disponibilité supprimée
+ *       404:
+ *         description: Disponibilité non trouvée
+ *       500:
+ *         description: Erreur serveur
+ */
 router.delete("/:id", deleteAvailability);
 
 module.exports = router;
